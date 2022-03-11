34afdad1233b9fcd307542c778e96625be672e49
Victor <vlcampos921@gmail.com>
2022-03-11 10:27:22 -0300

    Feat: Criando nova API falsa

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
+  animals: 644,
+  schedule: 644,
 });
 
 app.use(cors());
@@ -19,5 +21,4 @@ app.use(router);
 app.listen(port);
 
 console.log("Server is running on port:", port);
-
 /* A senha do Kenzinho é 123456 */
